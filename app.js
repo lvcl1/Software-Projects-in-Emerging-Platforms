0f383c1184299068516edfebffefebd07d1aee9c
leland <61434998+lvcl1@users.noreply.github.com>
2021-03-03 20:56:05 -0700

    changed the code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 const cameraView = document.getElementById("CameraView"),
-    cameraOutput = document.getElementById("CameraOutput"),
     cameraSensor = document.getElementById("CameraSensor"),
     cameraTrigger = document.getElementById("CameraTrigger")
 
@@ -21,8 +20,6 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
 };
 
 window.addEventListener("load", cameraStart, false);
